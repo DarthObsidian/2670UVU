5dda4727e8f3aa5ad74507fbed6944edbd9d63ac
MatthewMeyers <jedimastermeyers@gmail.com>
2017-08-25 14:01:28 -0600

    Created MoveInput Script

---
+++ Scripting2670/Assets/Scripts/MoveInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveInput : MonoBehaviour
+{
+    Action KeyAction;
+
+    // Use this for initialization
+    void Start ()
+    {
+        KeyAction = Move;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {         
+            KeyAction();
+        }
+	}
+
+    void Move()
+    {
+        print("Left Arrow");
+    }
+}

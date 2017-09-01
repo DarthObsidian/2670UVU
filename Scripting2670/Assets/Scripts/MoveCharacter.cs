32b4f5064d4b9be9d4ea4ce4bc6b2285c44e6437
MatthewMeyers <jedimastermeyers@gmail.com>
2017-09-01 16:39:10 -0600

    Added DoubleJump

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -9,6 +9,9 @@ public class MoveCharacter : MonoBehaviour
 	CharacterController cc;
 	Vector3 tempMove;
 	public float speed = 5;
+	public float gravity = 1;
+	public float jumpHeight = 0.2f;
+	int jumpCount = 0;
 
 	void Start () 
 	{
@@ -20,10 +23,27 @@ public class MoveCharacter : MonoBehaviour
 	{
 		MoveInput.KeyAction += Move;
 		PlayButton.Play -= OnPlay;
+		MoveInput.JumpAction = Jump;
 	}
+
 	void Move(float _movement)
 	{
+		tempMove.y -= gravity * Time.deltaTime;
 		tempMove.x = _movement * speed * Time.deltaTime;
 		cc.Move(tempMove);
+		
+		if(cc.isGrounded)
+		{
+			jumpCount = 0;
+		}
+	}
+
+	void Jump()
+	{
+		if(jumpCount < 2)
+		{
+			tempMove.y = jumpHeight;
+			jumpCount++;
+		}
 	}
 }

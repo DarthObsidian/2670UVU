ff0c315f3add948b65a8bb0a618d2bd8bf6bec54
Unknown <10796568@uvu.edu>
2017-09-02 13:59:19 -0600

    Created BearPrototypes

---
+++ Bear Prototypes/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class MoveCharacter : MonoBehaviour
+{
+	CharacterController cc;
+	Vector3 tempMove;
+	public float speed = 5;
+	public float gravity = 1;
+	public float jumpHeight = 0.2f;
+	int jumpCount = 0;
+
+	void Start () 
+	{
+		cc = GetComponent<CharacterController>();
+		MoveInput.KeyAction += Move;
+		MoveInput.JumpAction = Jump;
+	}
+
+	void Move(float _movement)
+	{
+		tempMove.y -= gravity * Time.deltaTime;
+		tempMove.x = _movement * speed * Time.deltaTime;
+		cc.Move(tempMove);
+		
+		if(cc.isGrounded)
+		{
+			jumpCount = 0;
+		}
+	}
+
+	void Jump()
+	{
+		if(jumpCount < 2)
+		{
+			tempMove.y = jumpHeight;
+			jumpCount++;
+		}
+	}
+}

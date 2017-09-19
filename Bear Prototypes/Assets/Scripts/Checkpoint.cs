c1385418a16faa04d1a9c8f6f962ba25dfd131a3
MatthewMeyers <10796568@uvu.edu>
2017-09-19 09:52:42 -0600

    Fixed Scripting Issues

---
+++ Bear Prototypes/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour 
+{
+	public Transform checkpoint;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			other.gameObject.transform.position = checkpoint.position;
+		}
+	}
+}
---
+++ Bear Prototypes/Assets/Scripts/FFloorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFloorController : MonoBehaviour 
+{
+	public Transform startTranform;
+
+	void Start()
+	{
+		//startTranform = gameObject.GetComponent<Transform>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			StartCoroutine("Fall");
+		}
+	}
+
+	IEnumerator Fall()
+	{
+		yield return new WaitForSeconds(0.5f);
+		gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		StartCoroutine("Reset");
+	}
+
+	IEnumerator Reset()
+	{
+		yield return new WaitForSeconds(5f);
+		gameObject.GetComponent<Rigidbody>().isKinematic = true;
+		gameObject.transform.position = startTranform.position;
+	}
+}

a52a11469f516343a98a3a4f280b6ae5fb2ffc8b
MatthewMeyers <jedimastermeyers@gmail.com>
2017-10-02 14:40:54 -0600

    created Spikes

---
+++ Bear Prototypes/Assets/Scripts/SpikeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour 
+{
+	public bool startState;
+	Animator anims;
+
+	void Start () 
+	{
+		anims = GetComponent<Animator>();
+		anims.SetBool("StartWithdrawn", startState);
+	}
+
+	void OnTriggerEnter()
+	{
+		anims.SetTrigger("Withdraw");
+	}
+
+	void onTriggerExit()
+	{
+		anims.SetTrigger("Withdraw");
+	}
+}

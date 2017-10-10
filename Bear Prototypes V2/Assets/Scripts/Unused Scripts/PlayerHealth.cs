92d70a2f2afec41ba7d02088163f7ca41a72199b
MatthewMeyers <jedimastermeyers@gmail.com>
2017-10-10 09:02:32 -0600

    Added Player Health

---
+++ Bear Prototypes V2/Assets/Scripts/Unused Scripts/PlayerHealth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public float power = 0.1f;
+
+	void OnTriggerEnter()
+	{
+		SendHealth.UpdateHealth(power);	
+	}
+}
---
+++ Bear Prototypes V2/Assets/Scripts/Unused Scripts/UpdateHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateHealthBar : MonoBehaviour 
+{
+	Vector3 scale = Vector3.one;
+	public Color good = Color.green;
+	public Color bad = Color.red;
+	private Image myRenderer;
+
+	void Start () 
+	{
+		SendHealth.HealthAction += ChangeHealthBar;
+	}
+
+	void ChangeHealthBar(float _health)
+	{
+		scale.x = _health;
+		transform.localScale = scale;
+
+		if(_health > 0.5)
+		{
+			myRenderer.color = good;
+		} else {
+			myRenderer.color = bad;
+		}
+	}
+}

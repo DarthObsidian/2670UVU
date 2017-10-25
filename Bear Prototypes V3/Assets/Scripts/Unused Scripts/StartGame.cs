d1d9fa002de1bd72fb67e1334d68f052f30de507
MatthewMeyers <jedimastermeyers@gmail.com>
2017-10-25 08:55:30 -0600

    Created build
    Also added UVU logo to splash screens

---
+++ Bear Prototypes V3/Assets/Scripts/Unused Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour 
+{
+	public Image mainMenu;
+
+	void Start() 
+	{
+		StaticVars.menuOpen = true;
+		Invoke("Begin", 1.0f);	
+	}
+
+	void Begin()
+	{
+		gameObject.SetActive(false);
+		mainMenu.gameObject.SetActive(true);
+	}
+
+}

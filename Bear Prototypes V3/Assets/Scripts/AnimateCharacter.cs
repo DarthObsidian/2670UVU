b70148fb3f2cf5ada589b2311c6e756545a955dc
MatthewMeyers <jedimastermeyers@gmail.com>
2017-11-14 14:12:36 -0700

    Began animating game

---
+++ Bear Prototypes V3/Assets/Scripts/AnimateCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateCharacter : MonoBehaviour 
+{
+
+	Animator anims;
+	bool firstStart = true;
+
+	void Start () 
+	{
+		anims = GetComponentInChildren<Animator>();
+	}
+	
+    private void Animate(float obj)
+    {
+
+    }
+}
